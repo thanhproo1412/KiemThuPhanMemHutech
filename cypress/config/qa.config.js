f0e123be4b71b5defcfbcde376365453d414c782
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2025-11-16 12:50:28 +0700

    delete all

--- cypress/config/qa.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    envName: "qa",
-    baseUrl: "https://speechanalyzer.elsaspeak.com", // ✅ Custom baseUrl
-    username: "qa-username",
-    password: "qa-password",
-  },
-};
--- cypress/support/supportFunction/writeJsonFile.js
+++
@@ -1,9 +0,0 @@
-// cypress/utils/fileUtils.js
-function writeJsonFile(filePath, data) {
-    cy.writeFile(filePath, data)
-        .then(() => {
-            cy.log(`File written successfully: ${filePath}`);
-        });
-}
-
-export default writeJsonFile;
